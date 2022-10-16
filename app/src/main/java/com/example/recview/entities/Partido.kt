834dd826e7a0caae73257fa74f1a06cf39cbe9ad
Osvaldo lucero <osvaldolcuero98@gmail.com>
2022-10-16 10:41:47 -0300

    Connected to firestore, VentaEntradas recycler does not render

--- app/src/main/java/com/example/recview/entities/Partido.kt
+++ app/src/main/java/com/example/recview/entities/Partido.kt
@@ -4,10 +4,21 @@ import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-class Partido (var id:Int,var equipo:String,var rival: String,var torneo: String) : Parcelable {
+class Partido (var id:Int, var equipo:String, var rival: String, var torneo: String) : Parcelable {
+
+    constructor() : this(0,"Velez","","")
 
     init {
-        this.id = 0
+        this.id = id
+        this.equipo = "Velez"
+        this.rival = rival!!
+        this.torneo = torneo!!
     }
 
+    fun testList(){
+
+    }
+    override fun toString(): String {
+        return "Partido(equipo='$equipo', rival='$rival', torneo='$torneo')"
+    }
 }
--- app/src/main/java/com/example/recview/viewmodels/VentaEntradasViewModel.kt
+++ app/src/main/java/com/example/recview/viewmodels/VentaEntradasViewModel.kt
@@ -1,7 +1,34 @@
 package com.example.recview.viewmodels
 
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import com.example.recview.entities.Partido
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
+import com.google.firebase.ktx.Firebase
 
 class VentaEntradasViewModel : ViewModel() {
     // TODO: Implement the ViewModel
+    private var partidos : MutableList<Partido> = mutableListOf()
+    // Access a Cloud Firestore instance from your Activity
+    private val db = Firebase.firestore
+
+    fun getPartidos() : MutableList<Partido>{
+        db.collection("partidos").get()
+            .addOnSuccessListener { snapshot ->
+                //TODO: LOG
+                if(snapshot != null){
+                    partidos.clear()
+                    for(partido in snapshot){
+                        partidos.add(partido.toObject())
+                    }
+                    Log.d("Partido" , partidos[0].toString())
+                }
+            }
+            .addOnFailureListener { exception ->
+                //TODO: LOG
+            }
+
+        return this.partidos
+    }
 }
