a7eb3d1a91ae38423685045f35d6548875147c63
ivanra48 <Blink.50>
2022-10-19 21:32:43 -0300

    Se agrega consulta de usuario para el carnet

--- app/src/main/java/com/example/recview/fragments/miPerfil/CarnetFragment.kt
+++ app/src/main/java/com/example/recview/fragments/miPerfil/CarnetFragment.kt
@@ -6,7 +6,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.navigation.findNavController
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.recview.R
+import com.example.recview.adapters.PartidosAdapter
+import com.example.recview.entities.Partido
+import com.example.recview.entities.User
+import com.example.recview.fragments.VentaEntradasFragmentDirections
 import com.example.recview.viewmodels.miPerfil.CarnetViewModel
 
 class CarnetFragment : Fragment() {
@@ -14,24 +21,33 @@ class CarnetFragment : Fragment() {
     companion object {
         fun newInstance() = CarnetFragment()
     }
+    lateinit var v: View
+    private var myAccount : MutableList<User> = ArrayList<User>()
 
-    private lateinit var viewModel: CarnetViewModel
+    private val viewModel: CarnetViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_carnet, container, false)
+       v = inflater.inflate(R.layout.fragment_carnet, container, false)
+        return v
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProvider(this).get(CarnetViewModel::class.java)
+       // viewModel = ViewModelProvider(this).get(CarnetViewModel::class.java)
         // TODO: Use the ViewModel
     }
 
-    override fun onCreate(savedInstanceState: Bundle?) {
+    /*override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+    }*/
+
+    override fun onStart() {
+        super.onStart()
+        myAccount = viewModel.getUser()
 
     }
 
--- app/src/main/java/com/example/recview/viewmodels/miPerfil/CarnetViewModel.kt
+++ app/src/main/java/com/example/recview/viewmodels/miPerfil/CarnetViewModel.kt
@@ -1,7 +1,36 @@
 package com.example.recview.viewmodels.miPerfil
 
+import android.content.ContentValues
+import android.util.Log
 import androidx.lifecycle.ViewModel
+import com.example.recview.entities.Partido
+import com.example.recview.entities.User
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.firestore.ktx.toObject
+import com.google.firebase.firestore.ktx.toObjects
+import com.google.firebase.ktx.Firebase
 
 class CarnetViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+
+    private var myAccount : MutableList<User> = mutableListOf()
+    private val db = Firebase.firestore
+
+    fun getUser() : MutableList<User> {
+        db.collection("usuarios")
+            .whereEqualTo("dni", 36397441)
+            .get()
+            .addOnSuccessListener { document ->
+                if (document != null) {
+                    myAccount.clear()
+                }
+                for(document in document) {
+                    myAccount.add(document.toObject<User>())
+                    Log.d("User", myAccount[0].toString())
+                }
+            }
+            .addOnFailureListener { exception ->
+                Log.w(ContentValues.TAG, "Error getting user profile: ", exception)
+            }
+        return myAccount
+    }
 }
