92edc45c3963537694786ff7b700f7ed5a72d6da
guidosolaro30 <guido02solaro@gmail.com>
2022-10-30 22:32:36 -0300

    User sin logica pero app funcionando para demo

--- app/src/main/java/com/example/recview/viewmodels/miPerfil/CarnetViewModel.kt
+++ app/src/main/java/com/example/recview/viewmodels/miPerfil/CarnetViewModel.kt
@@ -2,39 +2,45 @@ package com.example.recview.viewmodels.miPerfil
 
 import android.content.ContentValues
 import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import com.example.recview.entities.Partido
 import com.example.recview.entities.User
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.firestore.ktx.toObject
 import com.google.firebase.firestore.ktx.toObjects
 import com.google.firebase.ktx.Firebase
+import kotlinx.coroutines.launch
 import kotlinx.coroutines.tasks.await
 
 class CarnetViewModel : ViewModel() {
 
-    private var myAccount : MutableList<User> = mutableListOf()
+    //private var myAccount : MutableLiveData<MutableList<User>>()
+    private val myAccount = MutableLiveData<MutableList<User>>()
+
     private val db = Firebase.firestore
 
     // TODO: hay que agregar para que reciba el dni del usuario logueado
-    suspend fun getUser() : User {
-        val db = Firebase.firestore
+
+    fun getUser(){
+        viewModelScope.launch {
+            myAccount.value = getAll().toMutableList()
+        }
+    }
+    private suspend fun getAll() : MutableList<User> {
+        val userList = arrayListOf<User>()
         val userRef = db.collection("usuarios")
-        myAccount.clear()
         try {
-            val usuario = userRef.whereEqualTo("dni", 36397441).get().await()
-
-            if (myAccount != null) {
-                myAccount.clear()
-
-                for (document in usuario) {
-                    myAccount.add(document.toObject<User>())
-                    Log.d("User", myAccount[0].toString())
-                }
+            val data = userRef.get().await()
+            for (document in data) {
+                Log.d("User", userList[0].toString())
+                userList.add(document.toObject<User>())
             }
+
         } catch (e: Exception) {
             Log.w(ContentValues.TAG, "Error si ya compró ticket: ", e)
         }
-        return myAccount[0]
+        return userList
     }
 }
