baa76128857d410782b2a820b94cb5c57b1183ed
ivanra48 <Blink.50>
2022-11-01 17:50:24 -0300

    Se realiza funcion para envio de mail

--- app/src/main/java/com/example/recview/fragments/miPerfil/HaceteSocioFragment.kt
+++ app/src/main/java/com/example/recview/fragments/miPerfil/HaceteSocioFragment.kt
@@ -1,21 +1,31 @@
 package com.example.recview.fragments.miPerfil
 
-import androidx.lifecycle.ViewModelProvider
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
+import android.widget.TextView
+import androidx.fragment.app.Fragment
 import com.example.recview.R
 import com.example.recview.viewmodels.miPerfil.HaceteSocioViewModel
 import com.google.android.material.snackbar.Snackbar
+
 
 class HaceteSocioFragment : Fragment() {
 
 
     lateinit var v : View
     lateinit var btnHaceteSocio: Button
+    lateinit var haceteSocioNombre: TextView
+    lateinit var haceteSocioApellido: TextView
+    lateinit var haceteSocioMail: TextView
+    lateinit var haceteSocioDni: TextView
+    lateinit var haceteSocioCelular: TextView
+
+    private var correo: Array<String> = arrayOf("solicitudSocio@velez.com.ar")
 
     companion object {
         fun newInstance() = HaceteSocioFragment()
@@ -29,6 +39,11 @@ class HaceteSocioFragment : Fragment() {
     ): View? {
         v= inflater.inflate(R.layout.fragment_hacete_socio, container, false)
         btnHaceteSocio = v.findViewById(R.id.haceteSocioButton)
+        haceteSocioNombre = v.findViewById(R.id.haceteSocioNombre)
+        haceteSocioApellido = v.findViewById(R.id.haceteSocioApellido)
+        haceteSocioMail = v.findViewById(R.id.haceteSocioMail)
+        haceteSocioDni = v.findViewById(R.id.haceteSocioDni)
+        haceteSocioCelular = v.findViewById(R.id.haceteSocioCelular)
         return v
     }
 
@@ -36,7 +51,19 @@ class HaceteSocioFragment : Fragment() {
         super.onStart()
         btnHaceteSocio.setOnClickListener(){
             val contextView = v
+            val packageManager = requireActivity().packageManager
 
+            var solicitudSocio : String = haceteSocioNombre.text.toString() + "\n" + haceteSocioApellido.text.toString() +  "\n" + haceteSocioMail.text.toString() + "\n" + haceteSocioDni.text.toString() + "\n" + haceteSocioCelular.text.toString()
+
+            val intent = Intent(Intent.ACTION_SENDTO).apply {
+                data = Uri.parse("mailto:") // only email apps should handle this
+                putExtra(Intent.EXTRA_EMAIL, correo)
+                putExtra(Intent.EXTRA_SUBJECT, "solicitud de socio")
+                putExtra(Intent.EXTRA_TEXT, solicitudSocio)
+            }
+            if (intent.resolveActivity(packageManager) != null) {
+                startActivity(intent)
+            }
             Snackbar.make(contextView, "Tu solicitud ha sido enviada y esta sujeta a aprobacion", Snackbar.LENGTH_SHORT)
                 .show()
         }
