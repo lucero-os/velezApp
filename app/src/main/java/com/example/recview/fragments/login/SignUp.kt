fd1c8b78ce4cace04875e58e424d75a6bd127538
Osvaldo lucero <osvaldolcuero98@gmail.com>
2022-11-01 21:31:22 -0300

    Falta agregar users a auth y registro de user

--- app/src/main/java/com/example/recview/fragments/login/SignUp.kt
+++ app/src/main/java/com/example/recview/fragments/login/SignUp.kt
@@ -9,6 +9,7 @@ import android.view.ViewGroup
 import android.widget.Button
 import androidx.navigation.fragment.findNavController
 import com.example.recview.R
+import com.example.recview.databinding.FragmentSignUpBinding
 import com.example.recview.viewmodels.login.SignUpViewModel
 
 class SignUp : Fragment() {
@@ -16,18 +17,23 @@ class SignUp : Fragment() {
     companion object {
         fun newInstance() = SignUp()
     }
-
     private lateinit var viewModel: SignUpViewModel
-    private lateinit var v: View
-    private lateinit var registrarseBtn: Button
+//    private lateinit var v: View
+//    private lateinit var registrarseBtn: Button
+
+    //Implement binding
+    private var _binding : FragmentSignUpBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        v = inflater.inflate(R.layout.fragment_sign_up, container, false)
-        registrarseBtn = v.findViewById(R.id.registrarseButton)
-        return v
+//        v = inflater.inflate(R.layout.fragment_sign_up, container, false)
+//        registrarseBtn = v.findViewById(R.id.registrarseButton)
+//        return v
+        _binding = FragmentSignUpBinding.inflate(inflater, container, false)
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
@@ -39,10 +45,14 @@ class SignUp : Fragment() {
     override fun onStart() {
         super.onStart()
 
-        registrarseBtn.setOnClickListener {
-
+        binding.registrarseBtn.setOnClickListener {
             val action = SignUpDirections.actionSignUpToLogin()
             findNavController().navigate(action)
         }
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
--- app/src/main/java/com/example/recview/viewmodels/login/LoginViewModel.kt
+++ app/src/main/java/com/example/recview/viewmodels/login/LoginViewModel.kt
@@ -1,7 +1,27 @@
 package com.example.recview.viewmodels.login
 
+import android.content.ContentValues.TAG
+import android.util.Log
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LoginViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
+    private val auth: FirebaseAuth = Firebase.auth
+    var user : MutableLiveData<FirebaseUser?> = MutableLiveData(auth.currentUser)
+
+    fun login(email : String, pass : String) {
+        auth.signInWithEmailAndPassword(email, pass)
+            .addOnSuccessListener{
+                Log.d(TAG, "signInWithEmail:success")
+                user.value = auth.currentUser
+            }
+            .addOnFailureListener {
+                Log.d(TAG, "signInWithEmail:error")
+            }
+    }
+
 }
