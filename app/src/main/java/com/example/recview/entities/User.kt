e839223c26e169c53b73f1ab4b05bc24ed7b76c2
facusafe <facundosafe10@gmail.com>
2022-11-04 17:25:59 -0300

    saco id

--- app/src/main/java/com/example/recview/entities/User.kt
+++ app/src/main/java/com/example/recview/entities/User.kt
@@ -4,14 +4,13 @@ class User (
             var apellido: String? = "",
             var dni: Int? = 0,
             var email: String? = "",
-            var id: Int? = 0,
             var nombre: String? = "",
             var telefono: String? = ""
 ){
-    constructor() : this("",0,"",0,"","")
+    constructor() : this("",0,"","","")
 
     override fun toString(): String {
-        return "Usuario ($nombre, DNI='$dni')"
+        return "Usuario ($nombre)"
     }
 }
 
