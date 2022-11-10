94b500c95ff245ef1c4fbae50dcd9f7d35544461
ivanra48 <Blink.50>
2022-11-09 22:46:07 -0300

    Agrego validaciones de email y contraseña al login.

--- app/src/main/java/com/example/recview/viewmodels/login/LoginViewModel.kt
+++ app/src/main/java/com/example/recview/viewmodels/login/LoginViewModel.kt
@@ -6,6 +6,7 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.recview.entities.User
 import com.example.recview.entities.UserSingleton
+import com.google.api.Authentication
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.auth.ktx.auth
@@ -15,18 +16,22 @@ import com.google.firebase.ktx.Firebase
 
 class LoginViewModel : ViewModel() {
     private val auth: FirebaseAuth = Firebase.auth
-    private val db = Firebase.firestore
     var user : MutableLiveData<FirebaseUser?> = MutableLiveData(auth.currentUser)
 
     fun login(email : String, pass : String) {
-        auth.signInWithEmailAndPassword(email, pass)
-            .addOnSuccessListener{
-                Log.d(TAG, "signInWithEmail:success")
-                user.value = auth.currentUser
-            }
-            .addOnFailureListener {
-                Log.d(TAG, "signInWithEmail:error" + it)
-            }
+        try {
+            auth.signInWithEmailAndPassword(email, pass)
+                .addOnSuccessListener{
+                    Log.d(TAG, "signInWithEmail:success")
+                    user.value = auth.currentUser
+                }
+                .addOnFailureListener {
+                    Log.d(TAG, "signInWithEmail:error" + it)
+                    user.value = null
+                }
+        }catch (e: Exception){
+            Log.d("error", "no se pudo iniciar sesion")
+        }
     }
 
     fun logout(){
